c9f2bf45917ad7f48090c1d497fc32802981aee0
dhoangvudev <hoangvu1811.work@gmail.com>
2026-01-15 15:42:26 +0700

    chore: Remove upgrade to pro button, related files, .env.example, and external template links from various components.

--- src/components/layout/vertical/FooterContent.tsx
+++ src/components/layout/vertical/FooterContent.tsx
@@ -1,8 +1,5 @@
 'use client'
 
-// Next Imports
-import Link from 'next/link'
-
 // Third-party Imports
 import classnames from 'classnames'
 
@@ -14,6 +11,7 @@ import { verticalLayoutClasses } from '@layouts/utils/layoutClasses'
 
 const FooterContent = () => {
   // Hooks
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { isBreakpointReached } = useVerticalNav()
 
   return (
@@ -21,33 +19,8 @@ const FooterContent = () => {
       className={classnames(verticalLayoutClasses.footerContent, 'flex items-center justify-between flex-wrap gap-4')}
     >
       <p>
-        <span>{`© ${new Date().getFullYear()}, Made with `}</span>
-        <span>{`❤️`}</span>
-        <span>{` by `}</span>
-        <Link href='https://themeselection.com' target='_blank' className='text-primary'>
-          ThemeSelection
-        </Link>
+        <span>{`© ${new Date().getFullYear()} Your Company`}</span>
       </p>
-      {!isBreakpointReached && (
-        <div className='flex items-center gap-4'>
-          <Link href='https://themeselection.com/license' target='_blank' className='text-primary'>
-            License
-          </Link>
-          <Link href='https://themeselection.com' target='_blank' className='text-primary'>
-            More Themes
-          </Link>
-          <Link href={process.env.NEXT_PUBLIC_DOCS_URL as string} target='_blank' className='text-primary'>
-            Documentation
-          </Link>
-          <Link
-            href={`https://github.com/themeselection/${process.env.NEXT_PUBLIC_REPO_NAME}/issues`}
-            target='_blank'
-            className='text-primary'
-          >
-            Support
-          </Link>
-        </div>
-      )}
     </div>
   )
 }
