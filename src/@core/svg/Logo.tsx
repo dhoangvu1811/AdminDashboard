b143a1fe9d2cfd56ebe9df22d7beeeaee3012f52
dhoangvudev <hoangvu1811.work@gmail.com>
2026-05-19 15:43:50 +0700

    feat: initialize theme configuration, add project logo component, and update ignore list

--- src/@core/svg/Logo.tsx
+++ src/@core/svg/Logo.tsx
@@ -3,73 +3,17 @@ import type { SVGAttributes } from 'react'
 
 const Logo = (props: SVGAttributes<SVGElement>) => {
   return (
-    <svg width='1.2658em' height='1em' viewBox='0 0 100 79' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-      <path
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M4.92008 0.501904L22.662 11.4573C23.614 12.0451 24.1936 13.0844 24.1936 14.2036V64.2521C24.1936 65.3871 23.5976 66.4387 22.6241 67.0214L4.8822 77.6429C3.35344 78.5581 1.37254 78.0602 0.457741 76.5307C0.158194 76.0299 0 75.4572 0 74.8736V3.24818C0 1.46582 1.44424 0.0209274 3.22581 0.0209274C3.82422 0.0209274 4.41085 0.18746 4.92008 0.501904Z'
-        fill='currentColor'
-      />
-      <path
-        opacity='0.077704'
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M0 26.1063L24.1936 39.9852V53.5915L0 26.1063Z'
-        fill='black'
-      />
-      <path
-        opacity='0.077704'
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M0 26.1063L24.1936 39.6319V47.9438L0 26.1063Z'
-        fill='black'
-      />
-      <path
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M95.084 0.489601L77.3421 11.4083C76.3878 11.9956 75.8064 13.0362 75.8064 14.1571V64.2526C75.8064 65.3875 76.4024 66.4391 77.3759 67.0219L95.1178 77.6433C96.6466 78.5585 98.6275 78.0606 99.5423 76.5312C99.8418 76.0303 100 75.4576 100 74.874V3.23842C100 1.45605 98.5558 0.0111618 96.7742 0.0111618C96.1774 0.0111618 95.5923 0.176782 95.084 0.489601Z'
-        fill='currentColor'
-      />
-      <path
-        opacity='0.077704'
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M100 26.1063L75.8064 39.956V54.0023L100 26.1063Z'
-        fill='black'
-      />
-      <path
-        opacity='0.077704'
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M100 26.1063L75.8064 39.6199V48.3546L100 26.1063Z'
-        fill='black'
-      />
-      <path
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M4.91149 0.475694L50 28.123V54.7479L0 26.0986V3.22726C0 1.44489 1.44424 0 3.22581 0C3.8208 0 4.4042 0.164633 4.91149 0.475694Z'
-        fill='currentColor'
-      />
-      <path
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M4.91149 0.475694L50 28.123V54.7479L0 26.0986V3.22726C0 1.44489 1.44424 0 3.22581 0C3.8208 0 4.4042 0.164633 4.91149 0.475694Z'
-        fill='white'
-        fillOpacity='0.15'
-      />
-      <path
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M95.0885 0.475694L50 28.123V54.7479L100 26.0986V3.22726C100 1.44489 98.5558 0 96.7742 0C96.1792 0 95.5958 0.164633 95.0885 0.475694Z'
-        fill='currentColor'
-      />
-      <path
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M95.0885 0.475694L50 28.123V54.7479L100 26.0986V3.22726C100 1.44489 98.5558 0 96.7742 0C96.1792 0 95.5958 0.164633 95.0885 0.475694Z'
-        fill='white'
-        fillOpacity='0.3'
-      />
+    <svg width='1.5em' height='1.5em' viewBox='0 0 36 36' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+      <defs>
+        <linearGradient id="brandGrad" x1="0" y1="0" x2="36" y2="36" gradientUnits="userSpaceOnUse">
+          <stop offset="0%" stopColor="#3C50E0"/>
+          <stop offset="100%" stopColor="#8B5CF6"/>
+        </linearGradient>
+      </defs>
+      <rect x="0" y="0" width="36" height="36" rx="10" fill="url(#brandGrad)"/>
+      <path d="M12 14C12 10.6863 14.6863 8 18 8C21.3137 8 24 10.6863 24 14" stroke="white" strokeWidth="2.5" strokeLinecap="round"/>
+      <path d="M8 14C8 12.8954 8.89543 12 10 12H26C27.1046 12 28 12.8954 28 14V25C28 26.6569 26.6569 28 25 28H11C9.34315 28 8 26.6569 8 25V14Z" fill="white"/>
+      <path d="M18 15.5L19.2 18.2L22 19.5L19.2 20.8L18 23.5L16.8 20.8L14 19.5L16.8 18.2L18 15.5Z" fill="#3C50E0"/>
     </svg>
   )
 }
